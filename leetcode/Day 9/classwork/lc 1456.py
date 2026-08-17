37c87fdb103160c86b71e4d336c1b0cda73c467b
Amaan12Khan <khan.amaan1210@gmail.com>
2026-08-17 13:16:53 +0530

    Add new files

---
+++ leetcode/Day 9/classwork/lc 1456.py
@@ -0,0 +1,15 @@
+class Solution:
+    def maxVowels(self, s: str, k: int) -> int:
+        vowels = {'a', 'e', 'i', 'o', 'u'}
+        left = 0
+        count = 0
+        answer = 0
+        for right in range(len(s)):
+            if s[right] in vowels:
+                count += 1
+            if right - left + 1 == k:
+                answer = max(answer, count)
+                if s[left] in vowels:
+                    count -= 1
+                left += 1
+        return answer
